7a8e0247bc3aa6eddc53b38df66634a2e762c8d2
FortiBrine <FortiBrine@users.noreply.github.com>
2024-11-07 00:19:03 +0200

    add simple renderer

--- fabric/src/main/java/me/fortibrine/visualdriver/fabric/VisualDriver.java
+++ fabric/src/main/java/me/fortibrine/visualdriver/fabric/VisualDriver.java
@@ -1,12 +1,15 @@
 package me.fortibrine.visualdriver.fabric;
 
+import com.mojang.blaze3d.vertex.PoseStack;
 import lombok.Getter;
 import me.fortibrine.visualdriver.fabric.event.TitleScreenRenderCallback;
 import me.fortibrine.visualdriver.fabric.hud.HudManager;
 import me.fortibrine.visualdriver.fabric.listener.KeyListener;
 import me.fortibrine.visualdriver.fabric.listener.PacketListener;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.Minecraft;
+import net.minecraft.world.phys.Vec3;
 
 @Getter
 public class VisualDriver implements ClientModInitializer {
@@ -15,6 +18,27 @@ public class VisualDriver implements ClientModInitializer {
 
     @Override
     public void onInitializeClient() {
+        WorldRenderEvents.AFTER_ENTITIES.register(context -> {
+            PoseStack pose = context.matrixStack();
+            pose.pushPose();
+            Vec3 camera = context.camera().getPosition();
+            pose.translate(-camera.x, -camera.y, -camera.z);
+            pose.translate(100, 100, 100);
+            String text = "hello, world";
+            Minecraft.getInstance().font.drawInBatch(
+                    text,
+                    (float)(-Minecraft.getInstance().font.width(text) / 2),
+                    0,
+                    0xFF000000,
+                    true,
+                    pose.last().pose(),
+                    context.consumers(),
+                    true,
+                    0xFFFFFFFF,
+                    0
+            );
+            pose.popPose();
+        });
 
         TitleScreenRenderCallback.EVENT.register((screen, stack, info) -> {
             Minecraft.getInstance().font.drawShadow(
@@ -31,5 +55,4 @@ public class VisualDriver implements ClientModInitializer {
         new KeyListener();
 
     }
-
 }
