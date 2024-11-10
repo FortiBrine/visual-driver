2a3dce37542d42c6c9c87ddaf65de632d7fb220c
FortiBrine <FortiBrine@users.noreply.github.com>
2024-11-10 12:34:42 +0200

    add many hud render events

---
+++ fabric/src/main/java/me/fortibrine/visualdriver/fabric/event/HudRenderEvents.java
@@ -0,0 +1,137 @@
+package me.fortibrine.visualdriver.fabric.event;
+
+import com.mojang.blaze3d.vertex.PoseStack;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+
+public interface HudRenderEvents {
+
+    Event<RenderHealth> RENDER_HEALTH = EventFactory.createArrayBacked(
+            RenderHealth.class,
+            (listeners) -> (stack) -> {
+                for (RenderHealth listener : listeners) {
+                    if (!listener.renderHealth(stack)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+    );
+
+    Event<RenderVehicleHealth> RENDER_VEHICLE_HEALTH = EventFactory.createArrayBacked(
+            RenderVehicleHealth.class,
+            (listeners) -> (stack) -> {
+                for (RenderVehicleHealth listener : listeners) {
+                    if (!listener.renderVehicleHealth(stack)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+    );
+
+    Event<RenderEffects> RENDER_EFFECTS = EventFactory.createArrayBacked(
+            RenderEffects.class,
+            (listeners) -> (stack) -> {
+                for (RenderEffects listener : listeners) {
+                    if (!listener.renderEffects(stack)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+    );
+
+    Event<RenderExperienceBar> RENDER_EXPERIENCE_BAR = EventFactory.createArrayBacked(
+            RenderExperienceBar.class,
+            (listeners) -> (stack, i) -> {
+                for (RenderExperienceBar listener : listeners) {
+                    if (!listener.renderExperienceBar(stack, i)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+    );
+
+    Event<RenderJumpMeter> RENDER_JUMP_METER = EventFactory.createArrayBacked(
+            RenderJumpMeter.class,
+            (listeners) -> (stack, i) -> {
+                for (RenderJumpMeter listener : listeners) {
+                    if (!listener.renderJumpMeter(stack, i)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+    );
+
+    Event<RenderSelectedItemName> RENDER_SELECTED_ITEM_NAME = EventFactory.createArrayBacked(
+            RenderSelectedItemName.class,
+            (listeners) -> (stack) -> {
+                for (RenderSelectedItemName listener : listeners) {
+                    if (!listener.renderSelectedItemName(stack)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+    );
+
+    Event<RenderCrosshair> RENDER_CROSSHAIR = EventFactory.createArrayBacked(
+            RenderCrosshair.class,
+            (listeners) -> (stack) -> {
+                for (RenderCrosshair listener : listeners) {
+                    if (!listener.renderCrosshair(stack)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+    );
+
+    Event<RenderHotbar> RENDER_HOTBAR = EventFactory.createArrayBacked(
+            RenderHotbar.class,
+            (listeners) -> (f, stack) -> {
+                for (RenderHotbar listener : listeners) {
+                    if (!listener.renderHotbar(f, stack)) {
+                        return false;
+                    }
+                }
+                return true;
+            }
+    );
+
+    public static interface RenderHealth {
+        boolean renderHealth(PoseStack stack);
+    }
+
+    public static interface RenderVehicleHealth {
+        boolean renderVehicleHealth(PoseStack stack);
+    }
+
+    public static interface RenderEffects {
+        boolean renderEffects(PoseStack stack);
+    }
+
+    public static interface RenderExperienceBar {
+        boolean renderExperienceBar(PoseStack stack, int i);
+    }
+
+    public static interface RenderJumpMeter {
+        boolean renderJumpMeter(PoseStack stack, int i);
+    }
+
+    public static interface RenderSelectedItemName {
+        boolean renderSelectedItemName(PoseStack stack);
+    }
+
+    public static interface RenderCrosshair {
+        boolean renderCrosshair(PoseStack stack);
+    }
+
+    public static interface RenderHotbar {
+        boolean renderHotbar(float f, PoseStack stack);
+    }
+
+}
--- fabric/src/main/java/me/fortibrine/visualdriver/fabric/mixin/GuiMixin.java
+++ fabric/src/main/java/me/fortibrine/visualdriver/fabric/mixin/GuiMixin.java
@@ -1,17 +1,70 @@
 package me.fortibrine.visualdriver.fabric.mixin;
 
+import com.mojang.blaze3d.vertex.PoseStack;
+import me.fortibrine.visualdriver.fabric.event.HudRenderEvents;
 import net.minecraft.client.gui.Gui;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Gui.class)
 public class GuiMixin {
 
-    @ModifyVariable(method = "renderPlayerHealth", at = @At(value = "STORE"), ordinal = 12)
-    public int renderPlayerHealth(int v) {
-        // for disable rendering armor
-        return -1;
+    @Inject(method = "renderPlayerHealth", at = @At("HEAD"), cancellable = true)
+    public void renderPlayerHealth(PoseStack poseStack, CallbackInfo info) {
+        if (!HudRenderEvents.RENDER_HEALTH.invoker().renderHealth(poseStack)) {
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "renderVehicleHealth", at = @At("HEAD"), cancellable = true)
+    public void renderVehicleHealth(PoseStack poseStack, CallbackInfo info) {
+        if (!HudRenderEvents.RENDER_VEHICLE_HEALTH.invoker().renderVehicleHealth(poseStack)) {
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "renderEffects", at = @At("HEAD"), cancellable = true)
+    public void renderEffects(PoseStack poseStack, CallbackInfo info) {
+        if (!HudRenderEvents.RENDER_EFFECTS.invoker().renderEffects(poseStack)) {
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "renderExperienceBar", at = @At("HEAD"), cancellable = true)
+    public void renderExperienceBar(PoseStack poseStack, int i, CallbackInfo info) {
+        if (!HudRenderEvents.RENDER_EXPERIENCE_BAR.invoker().renderExperienceBar(poseStack, i)) {
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "renderJumpMeter", at = @At("HEAD"), cancellable = true)
+    public void renderJumpMeter(PoseStack poseStack, int i, CallbackInfo info) {
+        if (!HudRenderEvents.RENDER_JUMP_METER.invoker().renderJumpMeter(poseStack, i)) {
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "renderSelectedItemName", at = @At("HEAD"), cancellable = true)
+    public void renderSelectedItemName(PoseStack poseStack, CallbackInfo info) {
+        if (!HudRenderEvents.RENDER_SELECTED_ITEM_NAME.invoker().renderSelectedItemName(poseStack)) {
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "renderCrosshair", at = @At("HEAD"), cancellable = true)
+    public void renderCrosshair(PoseStack poseStack, CallbackInfo info) {
+        if (!HudRenderEvents.RENDER_CROSSHAIR.invoker().renderCrosshair(poseStack)) {
+            info.cancel();
+        }
+    }
+
+    @Inject(method = "renderHotbar", at = @At("HEAD"), cancellable = true)
+    public void renderHotbar(float f, PoseStack poseStack, CallbackInfo info) {
+        if (!HudRenderEvents.RENDER_HOTBAR.invoker().renderHotbar(f, poseStack)) {
+            info.cancel();
+        }
     }
 
 }
