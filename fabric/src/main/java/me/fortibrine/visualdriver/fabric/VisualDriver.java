2f06e995e8cf72da0fa3fac3c51344e9608f1265
FortiBrine <FortiBrine@users.noreply.github.com>
2024-12-04 23:27:43 +0200

    try to add external images

--- fabric/src/main/java/me/fortibrine/visualdriver/fabric/VisualDriver.java
+++ fabric/src/main/java/me/fortibrine/visualdriver/fabric/VisualDriver.java
@@ -1,6 +1,9 @@
 package me.fortibrine.visualdriver.fabric;
 
+import com.mojang.blaze3d.platform.NativeImage;
+import com.mojang.blaze3d.systems.RenderSystem;
 import lombok.Getter;
+import lombok.SneakyThrows;
 import me.fortibrine.visualdriver.fabric.gui.GuiPacketListener;
 import me.fortibrine.visualdriver.fabric.hud.HudManager;
 import me.fortibrine.visualdriver.fabric.hud.HudPacketListener;
@@ -11,7 +14,15 @@ import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayConnectionEvents;
 import net.fabricmc.fabric.api.client.screen.v1.ScreenEvents;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiComponent;
 import net.minecraft.client.gui.screens.TitleScreen;
+import net.minecraft.client.renderer.texture.DynamicTexture;
+import net.minecraft.client.renderer.texture.TextureManager;
+import net.minecraft.resources.ResourceLocation;
+
+import javax.imageio.ImageIO;
+import java.net.URI;
+import java.net.URL;
 
 @Getter
 public class VisualDriver implements ClientModInitializer {
@@ -19,6 +30,7 @@ public class VisualDriver implements ClientModInitializer {
     private final HudManager hudManager = new HudManager();
     private final WorldManager worldManager = new WorldManager();
 
+    @SneakyThrows
     @Override
     public void onInitializeClient() {
         ClientPlayConnectionEvents.DISCONNECT.register((handler, client) -> {
@@ -26,6 +38,12 @@ public class VisualDriver implements ClientModInitializer {
             hudManager.getDisableRender().clear();
             worldManager.getActions().clear();
         });
+
+//        NativeImage image = NativeImage.read(new URL("https://docs.fabricmc.net/assets/develop/rendering/draw-context-recipe-book-background.png").openStream());
+//        DynamicTexture texture = new DynamicTexture(image);
+//        Minecraft mc = Minecraft.getInstance();
+//        TextureManager textureManager = mc.getTextureManager();
+//        ResourceLocation location = textureManager.register("preview", texture);
 
         ScreenEvents.BEFORE_INIT.register((client, screen, scaledWidth, scaledHeight) -> {
             if (screen instanceof TitleScreen) {
@@ -37,6 +55,9 @@ public class VisualDriver implements ClientModInitializer {
                             10,
                             0xFFFFFF
                     );
+
+//                    textureManager.bind(location);
+//                    GuiComponent.blit(matrices, 90, 90, 0, 0, 16, 16, 16, 16);
                 }));
             }
         });
